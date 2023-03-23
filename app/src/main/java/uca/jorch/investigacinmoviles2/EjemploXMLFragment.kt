7cd8e4e5cdeb0d1f6e9f14570c75ac69da446eab
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-03-23 09:17:17 -0600

    4.0

--- app/src/main/java/uca/jorch/investigacinmoviles2/EjemploXMLFragment.kt
+++ app/src/main/java/uca/jorch/investigacinmoviles2/EjemploXMLFragment.kt
@@ -13,9 +13,8 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.android.volley.Request
 import uca.jorch.investigacinmoviles2.databinding.EjemploXmlBinding
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserException
-import org.xmlpull.v1.XmlPullParserFactory
+import uca.jorch.investigacinxml.Employee
+import uca.jorch.investigacinxml.XmlPullParserHandler
 import java.io.IOException
 
 
@@ -33,75 +32,25 @@ class EjemploXMLFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = EjemploXmlBinding.inflate(inflater, container, false)
         return binding.root
+
+        buscar()
     }
+    private fun buscar()
+    {
+        val listView = binding.listView
+        var employees: List<Employee>? = null
+        try {
+            val parser = XmlPullParserHandler()
+            val istream = assets.open("employees.xml")
+            employees = parser.parse(istream)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+            val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, employees)
+            listView.adapter = adapter
 
-        binding.buttonLoad.setOnClickListener {
-            //Manda a request usando la url ingresada
-            Toast.makeText(context, "Requesting...", Toast.LENGTH_SHORT).show()
-            cargarDatosDesdeUrl(binding.editTextUrl.text.toString())
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
-    private fun cargarDatosDesdeUrl(url: String) {
-        //Crea una una solicitud usando Volley
-        val queue = Volley.newRequestQueue(context)
-        Toast.makeText(context, "Queue Requested...", Toast.LENGTH_SHORT).show()
-
-        //Crea una solicitud GET para la URL proporcionada
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            { response -> //Devuelve la response del url
-                try {
-                    println(response)
-                    try {
-                        var datos = ""
-                        val factory = XmlPullParserFactory.newInstance()
-                        factory.isNamespaceAware = true
-                        val parser = factory.newPullParser()
-                        parser.setInput(response.reader())
-                        var eventType = parser.eventType
-                        while (eventType != XmlPullParser.END_DOCUMENT) {
-                            val tagname = parser.name
-                            when (eventType) {
-                                XmlPullParser.TEXT -> text = parser.text
-                                XmlPullParser.END_TAG -> if (tagname.equals("id", ignoreCase = true)) {
-                                    datos += "Id: \n"
-                                } else if (tagname.equals("name", ignoreCase = true)) {
-                                    datos += "Name: $text\n"
-                                } else if (tagname.equals("brand", ignoreCase = true)) {
-                                    datos += "Brand: $text\n"
-                                }
-                                /*else if (tagname.equals("year", ignoreCase = true)) {
-                                    datos += "Año: $text\n"
-                                } else if (tagname.equals("price", ignoreCase = true)) {
-                                    datos += "Precio: $text\n\n"
-                                }*/
-                                else -> {
-                                }
-                            }
-                            eventType = parser.next()
-                        }
-                        binding.textViewData.text = datos
-
-                    } catch (e: XmlPullParserException) {
-                        e.printStackTrace()
-                    } catch (e: IOException) {
-                        e.printStackTrace()
-                    }
-                    Toast.makeText(context, "Done...", Toast.LENGTH_SHORT).show()
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    Toast.makeText(context, "Error en la solicitud...", Toast.LENGTH_LONG).show()
-                }
-            }) { error -> println(error.message) }
-
-        queue.add(stringRequest)
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
+
+
