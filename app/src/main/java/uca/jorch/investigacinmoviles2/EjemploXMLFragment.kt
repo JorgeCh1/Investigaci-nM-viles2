dbaaad92323777e75f4959788b6ba91f78f08fe4
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-03-23 08:24:47 -0600

    Novo 2.0

--- app/src/main/java/uca/jorch/investigacinmoviles2/EjemploXMLFragment.kt
+++ app/src/main/java/uca/jorch/investigacinmoviles2/EjemploXMLFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SimpleAdapter
 import android.widget.Toast
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -14,6 +17,19 @@ import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserException
 import org.xmlpull.v1.XmlPullParserFactory
 import java.io.IOException
+import android.widget.ListAdapter
+import org.w3c.dom.Node
+import org.xml.sax.InputSource
+import org.xml.sax.SAXException
+import java.io.FileInputStream
+import java.io.InputStream
+import java.util.*
+import javax.xml.parsers.DocumentBuilderFactory
+import javax.xml.parsers.ParserConfigurationException
+import org.w3c.dom.Element
+import java.net.URL
+
+
 
 class EjemploXMLFragment : Fragment() {
     private var text: String? = null
@@ -21,63 +37,57 @@ class EjemploXMLFragment : Fragment() {
     private var _binding: EjemploXmlBinding? = null
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = EjemploXmlBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonLoad.setOnClickListener {
-
+            //Manda a request usando la url ingresada
             Toast.makeText(context, "Requesting...", Toast.LENGTH_SHORT).show()
             cargarDatosDesdeUrl(binding.editTextUrl.text.toString())
         }
     }
     private fun cargarDatosDesdeUrl(url: String) {
-
+        //Crea una una solicitud usando Volley
         val queue = Volley.newRequestQueue(context)
         Toast.makeText(context, "Queue Requested...", Toast.LENGTH_SHORT).show()
 
-
+        //Crea una solicitud GET para la URL proporcionada
         val stringRequest = StringRequest(
             Request.Method.GET, url,
-            { response ->
+            { response -> //Devuelve la response del url
                 try {
                     println(response)
                     try {
                         var datos = ""
-                        var factory = XmlPullParserFactory.newInstance()
+                        val factory = XmlPullParserFactory.newInstance()
                         factory.isNamespaceAware = true
-                        var parser = factory.newPullParser()
+                        val parser = factory.newPullParser()
                         parser.setInput(response.reader())
                         var eventType = parser.eventType
                         while (eventType != XmlPullParser.END_DOCUMENT) {
-                            var tagname = parser.name
+                            val tagname = parser.name
                             when (eventType) {
                                 XmlPullParser.TEXT -> text = parser.text
-                                XmlPullParser.END_TAG -> if (tagname.equals("clientId", ignoreCase = true)) {
-                                    datos += "idCliente: \n"
-                                } else if (tagname.equals("ContactDbId", ignoreCase = true)) {
-                                    datos += "idContactoDb: $text\n"
-                                } else if (tagname.equals("ContactId", ignoreCase = true)) {
-                                    datos += "idContacto: $text\n"
-                                } else if (tagname.equals("Errors", ignoreCase = true)) {
-                                    datos += "Errores: $text\n"
-                                } else if (tagname.equals("HasErrors", ignoreCase = true)) {
-                                    datos += "Errores: $text\n\n"
+                                XmlPullParser.END_TAG -> if (tagname.equals("id", ignoreCase = true)) {
+                                    datos += "Id: \n"
+                                } else if (tagname.equals("name", ignoreCase = true)) {
+                                    datos += "Name: $text\n"
+                                } else if (tagname.equals("brand", ignoreCase = true)) {
+                                    datos += "Brand: $text\n"
                                 }
+                                /*else if (tagname.equals("year", ignoreCase = true)) {
+                                    datos += "Año: $text\n"
+                                } else if (tagname.equals("price", ignoreCase = true)) {
+                                    datos += "Precio: $text\n\n"
+                                }*/
                                 else -> {
                                 }
                             }
                             eventType = parser.next()
                         }
                         binding.textViewData.text = datos
-
 
                     } catch (e: XmlPullParserException) {
                         e.printStackTrace()
